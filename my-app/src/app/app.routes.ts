87673f7c3e17a64d8d2cdc52a8f08f0b57fa69d0
jonathannicolas <hello@gmail.com>
2017-06-29 09:46:42 +0200

    Add link to shoes

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { RouterModule, Routes } from '@angular/router';
 import {Page1Component} from "./page1/page1.component";
-
+import {ShoeListComponent} from "./shoes/shoe-list/shoe-list.component"
 
 export const appRoutes: Routes = [
   { path: 'page1', component: Page1Component},
+  { path: 'shoes', component: ShoeListComponent},
 ];
